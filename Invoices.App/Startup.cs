75b59b47725c79e0a888e762268a9834481db0e1
SamHammersley <q5315908@live.tees.ac.uk>
2020-01-30 02:43:46 +0000

    Error page

--- Invoices.App/Startup.cs
+++ Invoices.App/Startup.cs
@@ -135,16 +135,9 @@ namespace Invoices.App
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+			app.UseDeveloperExceptionPage();
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
